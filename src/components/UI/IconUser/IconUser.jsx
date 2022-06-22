6d907b6b56ea4e9784f8890e1b67a7a970d31226
mrsrtAndrey <mrstr91@gmail.com>
2022-06-22 23:33:03 +0300

    make it better

--- src/components/UI/IconUser/IconUser.jsx
+++ src/components/UI/IconUser/IconUser.jsx
@@ -5,12 +5,12 @@ import userImg from '../../../assets/images/user-icon.webp'
 const IMG_URL = process.env.REACT_APP_GOOGLE_DRIVE_IMG_URL
 
 const IconUser = ({img, className, ...props}) => {
-   const [imgError, setImgError] = useState(!!img)
+   const [imgError, setImgError] = useState(!!!img)
    useEffect(()=>{
-      setImgError(!!img)
+      setImgError(!!!img)
    }, [img])
    return (
-      <img {...props} onError={()=>setImgError(false)} className={styles.icon+" "+className} src={imgError?IMG_URL+img:userImg} alt="User"/>
+      <img {...props} onError={()=>setImgError(false)} className={styles.icon+" "+className} src={imgError?userImg:IMG_URL+img} alt="User"/>
    );
 };
 export default IconUser
